101a61a4f95649b10b5ace49e8b0048f0ef4f8b5
saidwho12 <saidwho12@gmail.com>
2019-12-28 18:36:57 -0500

    Added matrix_float3x3.inl

--- glm/ext/matrix_float3x3.h
+++ glm/ext/matrix_float3x3.h
@@ -11,25 +11,11 @@ typedef union glm_float3x3
 } glm_float3x3, glm_mat3x3, glm_mat3;
 
 GLM_FUNC_QUALIFIER
-glm_float3x3 glm_transpose_float3x3(glm_float3x3 m)
-{
-	return (glm_float3x3)
-	{
-		m.e[0][0], m.e[1][0], m.e[2][0],
-		m.e[0][1], m.e[1][1], m.e[2][1],
-		m.e[0][2], m.e[1][2], m.e[2][2]
-	};
-}
+glm_mat3 glm_transpose_mat3(glm_mat3 m);
 
 GLM_FUNC_QUALIFIER
-glm_float3 glm_mul_float3x3_float3(glm_float3x3 m, glm_float3 v)
-{
-	//m = glm_transpose_float3x3(m);
-	glm_float3 s;
-	s.x = m.e[0][0] * v.x + m.e[1][0] * v.y + m.e[2][0] * v.z;
-	s.y = m.e[0][1] * v.x + m.e[1][1] * v.y + m.e[2][1] * v.z;
-	s.z = m.e[0][2] * v.x + m.e[1][2] * v.y + m.e[2][2] * v.z;
-	return s;
-}
+glm_vec3 glm_mul_mat3_vec3(glm_mat3 m, glm_vec3 v);
+
+#include "./matrix_float3x3.inl"
 
 #endif /* GLM_EXT_MATRIX_FLOAT3X3_H */
