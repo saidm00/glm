30ea17f7ca1f424f6223eb3d4ce3378e7c887dfd
saidwho12 <saidwho12@gmail.com>
2020-01-29 08:28:17 -0500

    Replaced static function pointers in setup.h by proper functions.

--- glm/detail/setup.h
+++ glm/detail/setup.h
@@ -7,7 +7,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-<<<<<<< HEAD
 typedef float glm_float32;
 typedef double glm_float64;
 
@@ -22,44 +21,6 @@ typedef int glm_int32;
 typedef long glm_int64;
 
 typedef unsigned int uint;
-=======
-#define GLM_ENABLE 1
-#define GLM_RIGHT_HANDED
-
-#include "../simd/platform.h"
-
-#if defined(GLM_FORCE_INLINE)
-#	if GLM_COMPILER & GLM_COMPILER_VC
-#		define GLM_INLINE __forceinline
-#		define GLM_NEVER_INLINE __declspec((noinline))
-#	elif GLM_COMPILER & (GLM_COMPILER_GCC | GLM_COMPILER_CLANG)
-#		define GLM_INLINE inline __attribute__((__always_inline__))
-#		define GLM_NEVER_INLINE __attribute__((__noinline__))
-#	elif GLM_COMPILER & GLM_COMPILER_CUDA
-#		define GLM_INLINE __forceinline__
-#		define GLM_NEVER_INLINE __noinline__
-#	else
-#		define GLM_INLINE inline
-#		define GLM_NEVER_INLINE
-#	endif/*GLM_COMPILER*/
-#else
-#	define GLM_INLINE inline
-#	define GLM_NEVER_INLINE
-#endif/*defined(GLM_FORCE_INLINE)*/
-
-#define GLM_FUNC_DECL GLM_CUDA_FUNC_DECL
-#define GLM_FUNC_QUALIFIER GLM_INLINE
-#	define GLM_NEVER_INLINE static
-#endif
-
-//#define GLM_FUNC_QUALIFIER static GLM_INLINE
-#define GLM_API GLM_INLINE
-
-#define GLM_ENABLE 1
-#define GLM_RIGHT_HANDED
-
-#define GLM_CONFIG_SIMD GLM_ENABLE
->>>>>>> 07172df52eedc227d46f7aa6adb083aeb7767f99
 
 #undef bool
 typedef _Bool bool;
@@ -80,27 +41,89 @@ enum glm_qualifier
     defaultp
 };
 
-inline double rsqrt(double x)
+inline double _sin(double x)
+{
+	return sin(x);
+}
+
+inline double _cos(double x)
+{
+	return cos(x);
+}
+
+inline double _tan(double x)
+{
+	return tan(x);
+}
+
+inline double _asin(double x)
+{
+	return asin(x);
+}
+
+inline double _acos(double x)
+{
+	return acos(x);
+}
+
+inline double _atan(double x)
+{
+	return atan(x);
+}
+
+inline double _atan2(double y, double x)
+{
+	return atan2(y, x);
+}
+
+inline double _sinh(double x)
+{
+	return sinh(x);
+}
+
+inline double _cosh(double x)
+{
+	return cosh(x);
+}
+
+inline double _tanh(double x)
+{
+	return tanh(x);
+}
+
+inline double _sqrt(double x)
+{
+	return sqrt(x);
+}
+
+inline double _inversesqrt(double x)
 {
 	return 1.0 / sqrt(x);
 }
 
-static double (*_sin)(double) = &sin;
-static double (*_cos)(double) = &cos;
-static double (*_tan)(double) = &tan;
-static double (*_asin)(double) = &asin;
-static double (*_acos)(double) = &acos;
-static double (*_atan)(double) = &atan;
-static double (*_atan2)(double, double) = &atan2;
-static double (*_sinh)(double) = &sinh;
-static double (*_cosh)(double) = &cosh;
-static double (*_tanh)(double) = &tanh;
-static double (*_sqrt)(double) = &sqrt;
-static double (*_inversesqrt)(double) = &rsqrt;
-static double (*_pow)(double, double) = &pow;
-static double (*_exp)(double) = &exp;
-static double (*_log)(double) = &log;
-static double (*_exp2)(double) = &exp2;
-static double (*_log2)(double) = &log2;
+inline double _pow(double x, double y)
+{
+	return pow(x, y);
+}
+
+inline double _exp(double x)
+{
+	return exp(x);
+}
+
+inline double _log(double x)
+{
+	return log(x);
+}
+
+inline double _exp2(double x)
+{
+	return exp2(x);
+}
+
+inline double _log2(double x)
+{
+	return log2(x);
+}
 
 #endif /* GLM_DETAIL_SETUP_H */
